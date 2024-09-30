4780d12ef6358d6135232e854f1e4ac87c1be642
zharfan-a <zharfan@walturn.com>
2024-09-30 20:27:00 +0700

    Update 15 files

---
+++ src/lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+import '../widgets/logout_button.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../cubits/auth_cubit.dart';
+
+class HomeScreen extends StatelessWidget {
+	const HomeScreen({Key? key}) : super(key: key);
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text('Home Screen'),
+			),
+			body: Center(
+				child: LogoutButton(),
+			),
+		);
+	}
+}
