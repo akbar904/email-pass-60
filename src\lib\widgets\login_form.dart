cac9785bf14ead4686b9f52f01cf1293eec9f22b
zharfan-a <zharfan@walturn.com>
2024-09-30 21:04:25 +0700

    Update 15 files

---
+++ src\lib\widgets\login_form.dart
@@ -0,0 +1,81 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../cubits/auth_cubit.dart';
+
+class LoginForm extends StatefulWidget {
+	const LoginForm({Key? key}) : super(key: key);
+
+	@override
+	_LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+	final _emailController = TextEditingController();
+	final _passwordController = TextEditingController();
+
+	@override
+	void dispose() {
+		_emailController.dispose();
+		_passwordController.dispose();
+		super.dispose();
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return BlocListener<AuthCubit, AuthState>(
+			listener: (context, state) {
+				if (state is AuthStateFailure) {
+					ScaffoldMessenger.of(context).showSnackBar(
+						SnackBar(
+							content: Text(state.message),
+						),
+					);
+				}
+			},
+			child: BlocBuilder<AuthCubit, AuthState>(
+				builder: (context, state) {
+					if (state is AuthStateLoading) {
+						return Center(
+							child: CircularProgressIndicator(),
+						);
+					}
+
+					return Padding(
+						padding: const EdgeInsets.all(16.0),
+						child: Column(
+							mainAxisAlignment: MainAxisAlignment.center,
+							children: [
+								TextField(
+									key: Key('emailField'),
+									controller: _emailController,
+									decoration: InputDecoration(
+										labelText: 'Email',
+									),
+									keyboardType: TextInputType.emailAddress,
+								),
+								TextField(
+									key: Key('passwordField'),
+									controller: _passwordController,
+									decoration: InputDecoration(
+										labelText: 'Password',
+									),
+									obscureText: true,
+								),
+								SizedBox(height: 16.0),
+								ElevatedButton(
+									onPressed: () {
+										final email = _emailController.text;
+										final password = _passwordController.text;
+										context.read<AuthCubit>().logIn(email, password);
+									},
+									child: Text('Login Yuk'),
+								),
+							],
+						),
+					);
+				},
+			),
+		);
+	}
+}
